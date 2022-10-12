c354c77dbfebda6628d48289a1b76624aa995b30
Atul <atul.yadav@fractal.ai>
2022-10-12 06:10:30 +0530

    with input from user

---
+++ main_user.py
@@ -0,0 +1,16 @@
+import fetch1
+import fetch2
+import datetime 
+now=datetime.datetime.now()
+
+# taking inputs --------
+apikey=input("API-key= ")
+start_with=input("name start with = ")
+
+now=now.strftime("%d/%m/%Y,%H:%M:%S")
+import hashlib
+hash = hashlib.md5( (now +"1dbd0287f2bf85c8ad18b654f3863fb131a57528"+ apikey).encode())
+print(hash.hexdigest())
+# print(now)
+# print(fetch1.fetch_data_api(0,now,"1b124b954b9c1a0f78740b153ae73b15",hash.hexdigest(),'a'))
+print(fetch2.fetch_data_api_name(now,"1b124b954b9c1a0f78740b153ae73b15",hash.hexdigest(),start_with))
