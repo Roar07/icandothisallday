4a3734e8c5b9c9e050538bd6a0b4e9b24dd68841
Atul <atul.yadav@fractal.ai>
2022-10-12 13:55:53 +0530

    with activity 4

--- fetch1.py
+++ fetch1.py
@@ -18,6 +18,19 @@ def fetch_data_api(offset,ts,key,Hash,name_start_with) :
     df1= pd.json_normalize(data)
     return df1[['id','name','comics.available','series.available','stories.available','events.available']]
 
+def filter_char(df,column,cond,comp):
+    if(cond==1):
+        return df[df[column]==comp]
+    if(cond==2):
+        return df[df[column]!=comp]
+    if(cond==3):
+        return df[df[column]<=comp]
+    if(cond==4):
+        return df[df[column]<comp]
+    if(cond==5):
+        return df[df[column]>=comp]
+    if(cond==6):
+        return df[df[column]>comp]
 
 # name = input('name')
 
--- main.py
+++ main.py
@@ -10,4 +10,19 @@ hash = hashlib.md5( (now +"1dbd0287f2bf85c8ad18b654f3863fb131a57528"+ '1b124b954
 print(hash.hexdigest())
 print(now)
 # print(fetch1.fetch_data_api(0,now,"1b124b954b9c1a0f78740b153ae73b15",hash.hexdigest(),'a'))
-print(fetch2.fetch_data_api_name(now,"1b124b954b9c1a0f78740b153ae73b15",hash.hexdigest(),'s'))
+
+df =fetch2.fetch_data_api_name(now,"1b124b954b9c1a0f78740b153ae73b15",hash.hexdigest(),'s')
+print(df)
+
+column = input("enter column to filter= ")
+cond = input("""choose from
+1. equal  to 
+2. not equal to 
+3. less than eqaul to 
+4. less than 
+5. greater than eqaul to 
+6. greater than
+choice = """)
+value= input (" enter value= ")
+
+print( fetch1.filter_char(df,column,cond,value))
